f57773f092cd791a24ac5ac57a574637a860df31
SONU RAJ <srajsonu02@gmail.com>
2020-02-24 17:17:48 +0530

    Initial Commit

---
+++ Smart Attendance System/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Attendance System
+//
+//  Created by ARY@N on 24/02/20.
+//  Copyright © 2020 Hitachi Payment Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
